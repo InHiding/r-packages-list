0f4fe48d8906eb219b951b7180783ed783a57083
Giuliano Melo <giuliano.melo@abril.com.br>
2015-11-06 10:46:16 -0200

    add relations to package

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,6 +2,6 @@ class Package < ActiveRecord::Base
   validates :name, presence: true
   validates :version, presence: true
 
-  has_many :authors
-  has_many :maintainers
+  has_many :authors, class_name: 'Developer'
+  has_many :maintainers , class_name: 'Developer'
 end
