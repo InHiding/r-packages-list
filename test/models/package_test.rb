59309de2bfbd07845fe5ae6546d0f1b1dc8b9bee
Giuliano Melo <giuliano.melo@abril.com.br>
2015-11-18 23:08:00 -0200

    update all

--- test/models/package_test.rb
+++ test/models/package_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class PackageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @package = Package.new
+  end
+
+  def teardown
+    @package = nil
+  end
+
+  test 'empty package' do
+    assert_not @package.save
+    assert_equal @package.errors.size, 2
+    assert_equal @package.errors.messages[:name].first, 'can\'t be blank'
+    assert_equal @package.errors.messages[:version].first, 'can\'t be blank'
+  end
+
+  test 'without name' do
+    @package.version = '0.0.1'
+    assert_not @package.save
+    assert_equal @package.errors.size, 1 
+    assert_equal @package.errors.messages[:name].first, 'can\'t be blank'
+  end
+
+  test 'without version' do
+    @package.name = 'A package'
+    assert_not @package.save
+    assert_equal @package.errors.size, 1 
+    assert_equal @package.errors.messages[:version].first, 'can\'t be blank'
+  end
 end
